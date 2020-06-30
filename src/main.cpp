49af13ea42d860601759b7e5c70af323ced904af
Unknown <unknown@example.com>
2020-06-30 00:52:37 -0400

    Added command line flags
    Features include silent mode (-s), "peek", and version messages. Also removed executables.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <string>
 
+// Switch for showing output of commands
+bool silent = false;
+
 // Utility functions
 void print();
 // https://stackoverflow.com/a/14267455
@@ -23,130 +26,89 @@ ADDRESS stack_ptr = 16;
 int stack[16] = {0};
 
 // Jump and math flags
-bool greater; // If the last jump comparison resulted in greater than
-bool less;    // If the last jump comparison resulted in less than
+// bool greater; // If the last jump comparison resulted in greater than
+// bool less;    // If the last jump comparison resulted in less than
 bool neg;     // If the last math operation would have resulted in a negative number
-bool zero;    // If the last jump comparison resulted in zero
+// bool zero;    // If the last jump comparison resulted in zero
 
 // Memory control flags
-bool mem_none; // If a move had no effect
+// bool mem_none; // If a move had no effect
 bool mem_out;  // If the stack is full
-bool mem_used; // If the default memory value is being used
+// bool mem_used; // If the default memory value is being used
 
 void mov(ADDRESS s, ADDRESS d);
 void psh(ADDRESS s);
 void pop(ADDRESS d);
 void add(ADDRESS s, ADDRESS d);
 void sub(ADDRESS s, ADDRESS d);
-void jmp(ADDRESS s);
-void jmpz(ADDRESS s, ADDRESS d);
-void jmpl(ADDRESS s, ADDRESS d);
-void jmpg(ADDRESS s, ADDRESS d);
+// void jmp(ADDRESS s);
+// void jmpz(ADDRESS s, ADDRESS d);
+// void jmpl(ADDRESS s, ADDRESS d);
+// void jmpg(ADDRESS s, ADDRESS d);
 
-#ifdef DEBUG
-void mov();
-void psh();
-void pop();
-void add();
-void sub();
-void jmp();
-void jpmz();
-void jpml();
-void jmpg();
-#endif
 
-int main()
+int main(int argc, char* argv[])
 {
-    while (input != "quit")
+    if (argc > 1) 
     {
-        std::getline(std::cin, input);
-        tokenize(input, delim);
-        if (tokens[0] == "place" || tokens[0] == "p")
-        {
-            // place value address
-            memory[stoi(tokens[2])] = stoi(tokens[1]);
-        }
-        if (tokens[0] == "mov" || tokens[0] == "m")
-        {
-// mov address_from address_to
-#ifdef DEBUG
-            if (tokens[1] == "" && tokens[2] == "")
-            {
-                mov();
-            }
-            else
-            {
-                mov(stoi(tokens[1]), stoi(tokens[2]));
-            }
-            continue;
-#endif
-            mov(stoi(tokens[1]), stoi(tokens[2]));
-        }
-        if (tokens[0] == "psh" || tokens[0] == "ps")
-        {
-// psh address_from
-#ifdef DEBUG
-            if (tokens[1] == "")
-            {
-                psh();
-            }
-            else
-            {
-                psh(stoi(tokens[1]));
-            }
-            continue;
-#endif
-            psh(stoi(tokens[1]));
-        }
-        if (tokens[0] == "pop" || tokens[0] == "pp")
-        {
-// pop address_to
-#ifdef DEBUG
-            if (tokens[1] == "")
-            {
-                pop();
-            }
-            else
-            {
-                pop(stoi(tokens[1]));
-            }
-            continue;
-#endif
-            pop(stoi(tokens[1]));
-        }
-        if (tokens[0] == "add" || tokens[0] == "a")
-        {
-// add address_from address_to
-#ifdef DEBUG
-            if (tokens[1] == "" && tokens[2] == "")
-            {
-                add();
-            }
-            else
-            {
-                add(stoi(tokens[1]), stoi(tokens[2]));
-            }
-            continue;
-#endif
-            add(stoi(tokens[1]), stoi(tokens[2]));
-        }
-        if (tokens[0] == "sub" || tokens[0] == "s")
-        {
-// sub address_from address_to
-#ifdef DEBUG
-            if (tokens[1] == "" && tokens[2] == "")
-            {
-                sub();
-            }
-            else
-            {
-                sub(stoi(tokens[1]), stoi(tokens[2]));
-            }
-            continue;
-#endif
-            sub(stoi(tokens[1]), stoi(tokens[2]));
-        }
-        print();
+        for (int i = 1; i < argc; i++)
+	{
+	    std::string arg = argv[i];
+	    if (arg == "-h")
+	    {
+	        // Display help message
+		return 0;
+	    } else if (arg == "-s")
+	    {
+	    	silent = true;
+	    } else if (arg == "-v")
+	    {
+	    	// Display version message
+		std::cout << "HASM Version 1.0, 6/29/2020" << std::endl;
+		return 0;
+	    }
+	}
+    }
+    while (input != "quit")
+    {	
+	if (!silent) { std::cout << "[HASM]: "; }
+	std::getline(std::cin, input);
+	tokenize(input, delim);
+	if (tokens[0] == "mov" || tokens[0] == "m")
+	{
+	    // mov address_from address_to
+	    mov(stoi(tokens[1]), stoi(tokens[2]));
+	}
+	if (tokens[0] == "psh" || tokens[0] == "ps")
+	{
+	    // psh address_from
+	    psh(stoi(tokens[1]));
+	}
+	if (tokens[0] == "pop" || tokens[0] == "pp")
+	{
+	    // pop address_to
+	    pop(stoi(tokens[1]));
+	}
+	if (tokens[0] == "add" || tokens[0] == "a")
+	{
+	    // add address_from address_to
+	    add(stoi(tokens[1]), stoi(tokens[2]));
+	}
+	if (tokens[0] == "sub" || tokens[0] == "s")
+	{
+	    // sub address_from address_to
+	    sub(stoi(tokens[1]), stoi(tokens[2]));
+	}
+	if (tokens[0] == "place" || tokens[0] == "p")
+	{
+	    // place value address
+	    memory[stoi(tokens[2])] = stoi(tokens[1]);
+	}
+	if (tokens[0] == "peek" || tokens[0] == "pe")
+	{
+	    if (silent) { print(); }
+	}
+	if (!silent) { print(); }
     }
     return 0;
 }
@@ -157,13 +119,13 @@ void print()
     std::cout << "Stack:  |";
     for (auto item : stack)
     {
-        std::cout << item << "|";
+	std::cout << item << "|";
     }
     std::cout << std::endl
-              << "Memory: |";
+	      << "Memory: |";
     for (auto item : memory)
     {
-        std::cout << item << "|";
+	std::cout << item << "|";
     }
     std::cout << std::endl;
 }
@@ -176,11 +138,11 @@ void tokenize(std::string const &str, std::string &delim)
     auto end = str.find(delim);
     while (end != std::string::npos)
     {
-        std::string current = str.substr(start, end - start);
-        tokens[i] = current;
-        i++;
-        start = end + delim.length();
-        end = str.find(delim, start);
+	std::string current = str.substr(start, end - start);
+	tokens[i] = current;
+	i++;
+	start = end + delim.length();
+	end = str.find(delim, start);
     }
     tokens[i] = str.substr(start, end);
 }
@@ -197,14 +159,14 @@ void psh(ADDRESS s)
     stack_ptr--;
     if (stack_ptr > 0)
     {
-        stack[stack_ptr] = memory[s];
-        return;
+	stack[stack_ptr] = memory[s];
+	return;
     }
     else
     {
-        mem_out = true;
-        stack_ptr++;
-        return;
+	mem_out = true;
+	stack_ptr++;
+	return;
     }
 }
 
@@ -214,12 +176,12 @@ void pop(ADDRESS d)
     stack[stack_ptr] = 0;
     if (stack_ptr < 16)
     {
-        stack_ptr++;
-        return;
+	stack_ptr++;
+	return;
     }
     else
     {
-        return;
+	return;
     }
 }
 
@@ -228,7 +190,7 @@ void add(ADDRESS s, ADDRESS d)
     memory[d] += memory[s];
     if (memory[d] < 0)
     {
-        neg = true;
+	neg = true;
     }
 }
 
@@ -237,86 +199,6 @@ void sub(ADDRESS s, ADDRESS d)
     memory[d] -= memory[s];
     if (memory[d] < 0)
     {
-        neg = true;
+	neg = true;
     }
 }
-
-#ifdef DEBUG
-void mov()
-{
-    mem_used = true;
-    pop(mem_default);     // Des
-    pop(mem_default + 1); // Src
-    memory[memory[mem_default]] = memory[memory[mem_default + 1]];
-    memory[memory[mem_default] - 1] = 0;
-    mem_used = false;
-    return;
-}
-
-void psh()
-{
-    mem_used = true;
-    pop(mem_default);
-    stack_ptr--;
-    if (stack_ptr > 0)
-    {
-        stack[stack_ptr] = memory[memory[mem_default]];
-        mem_used = false;
-        return;
-    }
-    else
-    {
-        mem_out = true;
-        mem_used = false;
-        stack_ptr++;
-        return;
-    }
-}
-
-void pop()
-{
-    mem_default = true;
-    pop(mem_default);
-    memory[memory[mem_default]] = stack[stack_ptr];
-    mem_default = false;
-    stack_ptr++;
-    if (stack_ptr >= 15) // Maybe change this to 16?
-    {
-        stack_ptr--;
-        mem_out = true;
-        return;
-    }
-    else
-    {
-        return;
-    }
-}
-
-void add()
-{
-    mem_used = true;
-    pop(mem_default);     // Des
-    pop(mem_default + 1); // Src
-    memory[memory[mem_default]] += memory[memory[mem_default + 1]];
-    if (memory[memory[mem_default + 1]] < 0)
-    {
-        neg = true;
-    }
-    mem_used = false;
-    return;
-}
-
-void sub()
-{
-    mem_used = true;
-    pop(mem_default);     // Des
-    pop(mem_default + 1); // Src
-    memory[memory[mem_default]] -= memory[memory[mem_default + 1]];
-    if (memory[memory[mem_default + 1]] < 0)
-    {
-        neg = true;
-    }
-    mem_used = false;
-    return;
-}
-#endif
